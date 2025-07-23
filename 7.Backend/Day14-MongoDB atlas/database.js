d133e477542029bc16a8a1d68fb57fad41f71703
Abhijeet <abhijeet.singh0513@gmail.com>
2025-07-23 17:20:00 +0530

    basic crud operations on db

---
+++ 7.Backend/Day14-MongoDB atlas/database.js
@@ -0,0 +1,49 @@
+// const url="mongodb+srv://admin100x:iOTzlOYLVj8jPNfv@100xdev.ktn5siu.mongodb.net/"
+
+const { MongoClient } = require("mongodb");
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url =
+  "mongodb+srv://admin100x:iOTzlOYLVj8jPNfv@100xdev.ktn5siu.mongodb.net/";
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = "CoderArmy";
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("user");
+
+  // the following code examples can be pasted here...
+
+    const findResult = await collection.find({}).toArray();
+    console.log("Found documents =>", findResult);
+
+    // to understand the above code throuch cursor which act as a pointer to avoid memory waste
+//   const findResult = collection.find({});
+
+//   for await (const doc of findResult) {
+//     console.log(doc);
+//   }
+
+// const insertResult = await collection.insertOne({name:"sanvi",age:1,city:"delhi"});
+// console.log('Inserted documents =>', insertResult);
+
+// const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+// console.log('Inserted documents =>', insertResult);
+
+const filteredDocs = await collection.find({ a: 3 }).toArray();
+console.log('Found documents filtered by { a: 3 } =>', filteredDocs);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
